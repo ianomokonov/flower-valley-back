2a8280aa351fdcc830bcc4f04895cd2542ed1778
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-01-17 23:06:23 +0300

    db

--- utils/database.php
+++ utils/database.php
@@ -12,6 +12,7 @@ class DataBase
         $this->db = new PDO("mysql:host=localhost;dbname=" . $this->dbname . ";charset=UTF8", $this->login, $this->password);
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     }
 
     public function genSelectQuery($table, $whereStmts = [])
