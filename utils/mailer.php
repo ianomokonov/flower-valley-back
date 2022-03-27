98fdcece1bd0294c02575dc19e4e99ce39a291e6
Ivan Volik <45817026+volik25@users.noreply.github.com>
2022-03-27 23:25:44 +0300

    Update mailer.php
    Add flowervalley smtp mail server

--- utils/mailer.php
+++ utils/mailer.php
@@ -20,15 +20,15 @@ class Mailer
             //Server settings
             // $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
             $this->mail->isSMTP();                                            //Send using SMTP
-            $this->mail->Host       = 'smtp.beget.com';                     //Set the SMTP server to send through
+            $this->mail->Host       = 'smtp.mail.ru';                     //Set the SMTP server to send through
             $this->mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $this->mail->Username   = 'flowervalley@progoff.ru';                     //SMTP username
-            $this->mail->Password   = 'QaZmLp1098';                               //SMTP password
-            $this->mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+            $this->mail->Username   = 'flowervalley@mail.ru';                     //SMTP username
+            $this->mail->Password   = 'r25qFNqRmdqRm21si2XM';                               //SMTP password
+            $this->mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
             $this->mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
             $this->mail->CharSet = 'UTF-8';
             //Recipients
-            $this->mail->From = 'flowervalley@progoff.ru';
+            $this->mail->From = 'flowervalley@mail.ru';
             $this->mail->FromName = 'Агрофима Цветочная Долина';
             //$this->mail->addAddress('lepingrapes@yandex.ru');
             // $mail->addCC('cc@example.com');
