7013cca27e93a8935c7df802f537d438d96158d4
Ivan Nomokonov <nomokonov.vana@yandex.ru>
2022-05-19 22:33:24 +0300

    fix

--- utils/sorter.php
+++ utils/sorter.php
@@ -3,7 +3,7 @@
 function sortItems($items, $table, $orderColName, $dataBase)
 {
     foreach ($items as $item) {
-        $query = "update $table set $orderColName=? where id=?";
+        $query = "update $table set `$orderColName`=? where id=?";
         $stmt = $dataBase->db->prepare($query);
         $stmt->execute(array($item[$orderColName], $item['id']));
     }
